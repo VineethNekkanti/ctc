061226d86b9e214ae96dc1b8e664b7f275c9793d
Vineeth <NVineeth@its.jnj.com>
2022-04-30 13:28:00 +0530

    Fix Nav and null form

--- public/nav.js
+++ public/nav.js
@@ -1,14 +1,50 @@
 const navList = document.querySelector('.nav-list');
 const toggle = document.querySelector('.nav-mobile-toggle');
-const visibility = navList.getAttribute("data-visible");
+const aboutUS = document.querySelector('.anchor1');
+const writeAReview = document.querySelector('.anchor2');
+const contribute = document.querySelector('.anchor3');
+
 
 toggle.addEventListener('click', () => {
+const visibility = navList.getAttribute("data-visible");
 
   if(visibility === "false"){
     navList.setAttribute("data-visible", true);
     toggle.setAttribute("aria-expanded", true);
+
   } else if(visibility === "true") {
     navList.setAttribute("data-visible", false);
     toggle.setAttribute("aria-expanded", false);
+
   }
 });
+
+aboutUS.addEventListener('click', () => {
+const visibility = navList.getAttribute("data-visible");
+
+  if(visibility === "true") {
+    navList.setAttribute("data-visible", false);
+    toggle.setAttribute("aria-expanded", false);
+  }
+
+})
+
+writeAReview.addEventListener('click', () => {
+const visibility = navList.getAttribute("data-visible");
+
+  if(visibility === "true") {
+    navList.setAttribute("data-visible", false);
+    toggle.setAttribute("aria-expanded", false);
+  }
+
+})
+
+contribute.addEventListener('click', () => {
+const visibility = navList.getAttribute("data-visible");
+
+  if(visibility === "true") {
+    navList.setAttribute("data-visible", false);
+    toggle.setAttribute("aria-expanded", false);
+  }
+
+})
